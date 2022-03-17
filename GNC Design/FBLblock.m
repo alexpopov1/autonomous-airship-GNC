8a82f8b6be5ad74f2d31d99dbd6c4778f8d167b0
Alexander Popov <65553615+alexpopov1@users.noreply.github.com>
2022-03-17 15:45:05 +0000

    Update FBLblock.m

--- GNC Design/FBLblock.m
+++ GNC Design/FBLblock.m
@@ -16,19 +16,19 @@
 %               omega_i^2*sin(alpha_i) and omega_i^2*cos(alpha_i)
 
 function real_input = FBLblock(transform_input,states)
-%% Import function handles for ALPHA and BETA matrices
+% Import function handles for ALPHA and BETA matrices
 load('feedback_linearisation')
 
-%% Set numerical inputs of parameters
+% Set numerical inputs of parameters
 [k_f,k_t,L,d,mass,mass_add,g,Ixx,Iyy,Izz,Ixz,Buoy,CD] = parameters;
 
-%% Define required elements of state vector
+% Define required elements of state vector
 u = states(1); v = states(2); w = states(3); p = states(4); q = states(5); 
 r = states(6); phi = states(7); theta = states(8); psi = states(9);  
 
-%% Apply numerical inputs to symbolic functions to obtain ALPHA and BETA
+% Apply numerical inputs to symbolic functions to obtain ALPHA and BETA
 ALPHA = alpha_function(Buoy,CD,Ixx,Ixz,Iyy,Izz,g,mass,mass_add,p,phi,psi,q,r,theta,u,v,w);
 BETA = beta_function(Ixx,Ixz,Iyy,Izz,L,d,k_f,k_t,mass,mass_add,phi,psi,theta);
 
-%% Real input (u = rho)
+% Real input (u = rho)
 real_input = inv(BETA)*(transform_input - ALPHA);
