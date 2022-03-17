9c415ef3f23f5e9fbc04368baf4373690b1abc69
Alexander Popov <65553615+alexpopov1@users.noreply.github.com>
2022-03-17 15:44:31 +0000

    Update dynamicsblock.m

--- GNC Design/dynamicsblock.m
+++ GNC Design/dynamicsblock.m
@@ -18,8 +18,6 @@ u = states(1); v = states(2); w = states(3); p = states(4); q = states(5);
 r = states(6); phi = states(7); theta = states(8); psi = states(9);  
 
 % Return numerical values for the 12 state derivatives
-states_dot = functions(Buoy,CD,Ixx,Ixz,Iyy,Izz,L,d,g,...
-    input(1),input(2),input(3),input(4),input(5),input(6),...
-    k_f,k_t,mass,mass_add,p,phi,psi,q,r,theta,u,v,w);
+states_dot = functions(Buoy,CD,Ixx,Ixz,Iyy,Izz,L,d,g, input(1),input(2),input(3),input(4),input(5),input(6),k_f,k_t,mass,mass_add,p,phi,psi,q,r,theta,u,v,w);
 
 end
